c6806193e6e9bc2d2ca47cb5a8b3ef8b52ea1a2d
Benjamin Holfve <benjamin@trevald.com>
2024-03-15 14:33:50 +0100

    Added settings for Intervall

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <template>
     <AppDisplay :mode="mode" :value="value" />
+    <AppSettings :settings="settings" @change="updateSettings" />
 </template>
 
 <script>
 import AppDisplay from "./components/AppDisplay.vue";
+import AppSettings from "./components/AppSettings.vue";
 
 export default {
-    components: { AppDisplay },
+    components: { AppDisplay, AppSettings },
 
     data() {
         return {
             mode: "number", // number|color
-            intervall: 3000, // in ms
+            intervall: 3, // in ms
             currentNumber: undefined,
             currentColor: undefined,
             minNumber: 1,
-            maxNumber: 99,
+            maxNumber: 9,
         };
     },
 
     computed: {
+        settings() {
+            return {
+                mode: this.mode,
+                intervall: this.intervall,
+                minNumber: this.minNumber,
+                maxNumber: this.maxNumber,
+            };
+        },
+
         value() {
             switch (this.mode) {
                 case "number":
@@ -43,7 +54,7 @@ export default {
         },
 
         setTimer() {
-            setTimeout(this.setNumber, this.intervall);
+            setTimeout(this.setNumber, this.intervall * 1000);
         },
 
         start() {
@@ -51,8 +62,21 @@ export default {
         },
 
         setNumber() {
-            this.currentNumber = this.getRandomNumber();
+            const newNumber = this.getRandomNumber();
+            if (newNumber === this.currentNumber) {
+                this.setNumber();
+                return;
+            }
+            this.currentNumber = newNumber;
+
             this.setTimer();
+        },
+
+        updateSettings(data) {
+            console.log(data);
+            this.intervall = data.intervall;
+            this.minNumber = data.minNumber;
+            this.maxNumber = data.maxNumber;
         },
     },
 
--- src/components/AppSettings.vue
+++ src/components/AppSettings.vue
@@ -1,7 +1,133 @@
 <template>
-    <div class="app-settings"></div>
+    <button @click="toggle" type="button" class="button-settings">
+        Settings
+    </button>
+    <div class="app-settings" v-if="active">
+        <div class="content">
+            <div class="form-group">
+                <label for="input-intervall"
+                    >Intervall ({{ intervall }}s)</label
+                >
+                <input
+                    type="range"
+                    min="1"
+                    max="10"
+                    step="1"
+                    v-model="intervall"
+                    @input="handleChange"
+                />
+            </div>
+        </div>
+
+        <div class="actions">
+            <button @click="toggle" type="button">Done</button>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+    props: ["settings"],
+    emits: ["change"],
+
+    data() {
+        return {
+            mode: this.settings.mode,
+            intervall: this.settings.intervall,
+            minNumber: this.settings.minNumber,
+            maxNumber: this.settings.maxNumber,
+            active: false,
+        };
+    },
+
+    methods: {
+        handleChange($e) {
+            this.$emit("change", {
+                mode: this.mode,
+                intervall: this.intervall,
+                minNumber: this.minNumber,
+                maxNumber: this.maxNumber,
+            });
+        },
+
+        toggle() {
+            this.active = !this.active;
+        },
+    },
+};
 </script>
+
+<style scoped>
+.app-settings {
+    position: fixed;
+    z-index: 10;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    color: rgba(255, 255, 255, 0.8);
+    background-color: #242429;
+}
+
+.content {
+    padding: 2vw;
+    flex: 0 0 auto;
+}
+
+.actions {
+    padding: 4vw 2vw;
+    display: flex;
+    flex: 0 0 auto;
+    justify-content: center;
+}
+
+.form-group {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2vw;
+}
+
+label {
+    font-size: 3vw;
+    font-weight: normal;
+    text-align: center;
+}
+
+input[type="range"] {
+    width: 80%;
+}
+
+button {
+    appearance: none;
+    border: none;
+    box-shadow: none;
+    border-radius: 100vw;
+    background-color: rgb(0, 100, 255);
+    font-weight: bold;
+    color: white;
+    padding: 2vw 4vw;
+    min-width: 20vw;
+    font-size: 2vw;
+}
+
+.button-settings {
+    position: fixed;
+    z-index: 5;
+    bottom: 1rem;
+    right: 1rem;
+
+    font-size: 1rem;
+    padding: 1rem;
+    min-width: unset;
+
+    color: rgba(255, 255, 255, 0.4);
+    background-color: rgba(255, 255, 255, 0.1);
+}
+</style>
